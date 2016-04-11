8351f7a2ee11baa91db173cf2703bb645309843e
thibaut2g <thibs2g@gmail.com>
2016-04-11 17:09:57 +0200

    Ajout du CSS et du JS Début de création de la page d'accueil :p

---
+++ footer.php
@@ -0,0 +1,29 @@
+</main>
+<footer class="page-footer indigo darken-3">
+    <div class="footer-copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col s6 left-align">
+          Thibaut et Cyrille</div>
+        <div class="col s6 right-align">
+         <!--  <a href="backoffice/homepage.php">Backoffice</a> -->
+        </div>
+      </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="/Experiment/js/materialize.js"></script>
+  <script src="/Experiment/js/init.js"></script>
+   <script type="text/javascript" charset="utf-8">
+   
+  $(".button-collapse").sideNav();
+        $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+</script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-hello cyrille 2cro!
+<?php include "header.php" ?>
+<div class="top"></div>
+
+<div class="container">
+	<div class="row">
+		<div class="col s12 l6 center-align">
+			<a class="waves-effect waves-light btn-large indigo darken-3">Connexion</a>
+		</div>
+		<div class="col s12 l6 center-align">
+			<a class="waves-effect waves-light btn-large indigo darken-3">Inscription</a>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col s12 l6 center-align">
+			<div class="top">
+				<p>Hello dear</p>
+			</div>
+			<div class="top"></div>
+		</div>
+	</div>
+</div>
+
+<?php include "footer.php" ?>
