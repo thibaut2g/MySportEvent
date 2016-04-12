5e99f04c0cbc31697b0cd9c8a95d3e5ca306025a
thibaut2g <thibs2g@gmail.com>
2016-04-12 14:00:28 +0200

    Mise à jour du design de la page d'accueil ;)

--- index.php
+++ index.php
@@ -10,12 +10,16 @@
 			<a class="waves-effect waves-light btn-large indigo darken-3">Inscription</a>
 		</div>
 	</div>
-	<div class="principal">
+	<div class="top"></div>
+</div>
+<div class="container">
 	<div class="row">
-		<div class="col s12 l6 center-align">
-			<div class="top">
-				<p>Hello dear</p>
-			</div>
+		<div class="col s12 center-align">
+			<div class="top"></div>
+			<div class="top"></div>
+			<div class="top"></div>
+				<p class="flow-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod asperiores repudiandae, eum nemo voluptas dolores, at est similique, debitis dicta minus suscipit dolorum modi! Esse iste ipsam excepturi sed aliquid!</p>
+			
 			<div class="top"></div>
 		</div>
 	</div>
