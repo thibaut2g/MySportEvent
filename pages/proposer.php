3074df45c3bf3b43a7aa600304a9a9771b6ab724
massue2cro <cyrille.decrocq@hotmail.fr>
2016-04-20 16:35:40 +0200

    graphique formulaire événement terminé

--- pages/proposer.php
+++ pages/proposer.php
@@ -1,26 +1,30 @@
-<?php include "header.php" ?>
-	<div class="container">
-		<div class="row">
+	<div class="container" >
+		<div class="row " style="margin-bottom:0%">
 
 			<form class="col s12">
 					<div class="top"></div>
 			      <div class="row">
-			      <h1>Nouvel événement</h1>
+			      <h1 class="white-text center z-depth-2 indigo darken-3">Nouvel événement </h1>
 			        <div class="input-field col s6">
 			          <input id="titre" type="text" class="validate">
 			          <label for="titre">Titre</label>
 			        </div>
-
+			        <br><br/> <br><br/>
 			        <div class="input-field col s12">
 			          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
 			          <label for="icon_prefix2">Description</label>
+			          <br><br/>
 			        </div>
-			        
 			        <div class="col s3">
 				  			<label for="date">Date</label>
-				  			 <input id="date" type="date" class="datepicker"><input  type="time" class="timepicker">
-				  	</div>	
+				  			<input id="date" type="date" class="datepicker">
+				  			<br><br/> 
+				  			<label for="time">Heure</label>
+				  			<input id="time"  type="time">
+				  	</div>
+				  	
 				  		<div class="col s12">
+				  		<br><br/> 
 				  		<label for="icon_prefix2">Discipline sportive</label>
 						<p>
 					      <input name="group1" type="radio" id="test1" />
@@ -30,12 +34,15 @@
 					      <input name="group1" type="radio" id="test2" />
 					      <label for="test2">Football</label>
 					    </p>
+					     <br><br/>
 			        </div>
+			       
 			        <div class="col s12">
 			        <label for="icon_prefix2">Nombre maximal de joueurs</label>
 			        	<p class="range-field">
       						<input type="range" id="test5" min="0" max="50" />
     					</p>
+    					<br><br/>
 			        </div>
 			        <div class="col s12">
 			        <label for="icon_prefix2">Photo de l'événement</label>
@@ -47,12 +54,13 @@
 					      <div class="file-path-wrapper">
 					        <input class="file-path validate" type="text">
 					      </div>
+					      <br><br/>
 					    </div>
 					</div>
-				    </div>
+				    </div>		      	
+				    <input class="large btn-large indigo darken-3" type="submit" value="Je créé mon événement !" style="margin-bottom: 3%;" />
 			      </div>
-		      	<input class="large btn-large indigo darken-3" type="submit" value="Ok !" />
+
 			</form>
 		</div>
 	</div>
-<?php include "footer.php" ?>
